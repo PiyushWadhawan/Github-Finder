2fce963db5afe0518411fb7781712e1184d88535
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2023-11-19 04:11:22 +0530

    resolved more bugs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { BiArrowBack } from "react-icons/bi";
 import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <div className="about">
+      <h3 className="back_home">
+        <Link to="/" className="back_home">
+          <span>
+            <BiArrowBack />
+          </span>
+        </Link>
+      </h3>
       <h1>Github Finder</h1>
       <h3>
         This is an application made using React to search for github profiles
